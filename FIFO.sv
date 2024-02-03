3097aa992382ccc0c2cb715f88841a78091d5a9f
Daniyal Ahmad <daniyal@pdx.edu>
2024-02-02 20:54:59 -0800

    pushing initial template of FIFO

---
+++ FIFO.sv
@@ -0,0 +1,18 @@
+module AsyncFIFO (
+    input wire clk_write,
+    input wire clk_read,
+    input wire rst,
+    input wire [7:0] data_in,
+    input wire write_enable,
+    input wire read_enable,
+    output logic [7:0] data_out,
+    output logic full,
+    output logic empty,
+    output logic almost_full,
+    output logic almost_empty
+):
+
+
+
+endmodule
+
